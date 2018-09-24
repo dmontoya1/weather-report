50a4f6f255b207476c2ddea1be9965ae637317a4
Daniel Felipe Montoya Ramirez <dmontoya@apptitud.com.co>
2018-09-23 22:02:22 -0500

    Initial commit

---
+++ weather-app/src/components/WeatherData.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import WeatherExtraInfo from './WeatherExtraInfo';
+import WeatherTemperature from './WeatherTemperature';
+import {
+    CLOUD,
+    CLOUDY,
+    SUN,
+    RAIN,
+    SNOW,
+    WINDY
+} from './../constants/weathers'
+
+const WeatherData = () => (
+    <div>
+        <WeatherTemperature 
+            temperature={20} 
+            weatherState={SUN} 
+        />
+        <WeatherExtraInfo 
+            humidity={25} 
+            wind={'10 m/s'} 
+        />
+    </div>
+);
+
+export default WeatherData;
---
+++ weather-app/src/components/WeatherLocation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Location from './Location'
+import WeaterData from './WeatherData'
+
+
+const WeatherLocation = () => (
+   <div>
+       <Location city={'Pereira'}></Location>
+       <WeaterData></WeaterData>
+   </div>
+);
+
+export default WeatherLocation;
